4f23eb0977595ba4d937292a1112802115d92b41
大島 亮一 <or004610@taiyokogyo.co.jp>
2026-01-09 14:46:36 +0900

    Initial commit: 現場スキャンWebアプリ一式

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'field-scan-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+  'https://unpkg.com/html5-qrcode'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
